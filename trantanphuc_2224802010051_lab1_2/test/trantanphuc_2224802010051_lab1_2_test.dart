8ac670937e361fbfa593a0c13e42e65ff3a23bbb
Fate <trantanphuc1105@gmail.com>
2026-03-31 14:04:46 +0700

    lab1_2

---
+++ trantanphuc_2224802010051_lab1_2/test/trantanphuc_2224802010051_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:trantanphuc_2224802010051_lab1_2/trantanphuc_2224802010051_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
